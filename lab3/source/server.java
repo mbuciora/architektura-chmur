b6acfcc2e9f2ccb0ef5c29469b600e59040f7c66
Marcin Buciora <buciora.marcin@gmail.com>
2022-11-26 10:20:36 +0100

    lab3

---
+++ lab3/source/server.java
@@ -0,0 +1,72 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.text.SimpleDateFormat;  
+import java.util.Date;  
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+import java.util.HashMap;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+public class Test {
+
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(4080), 0);
+        server.createContext("/", new MyHandler());
+        server.setExecutor(null); // creates a default executor
+        server.start();
+    }
+
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            Map<String, String> params = queryToMap(t.getRequestURI().getQuery()); 
+            String response = "";
+
+            if (params == null) {
+                response = "Hello World from java!\n";
+            } else if (params.get("cmd").equals("time")) {
+                SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");  
+                Date date = new Date(); 
+                response = formatter.format(date);
+            } else if (params.get("cmd").equals("rev")) {
+                String str = params.get("str");
+            	String nstr = "";
+            	for (int i=0; i<str.length(); i++) {
+                    char ch= str.charAt(i);
+                    nstr= ch+nstr;
+                }
+                response += nstr;
+            }
+
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+
+        static public Map<String, String> queryToMap(String query) {
+            if(query == null) {
+                return null;
+            }
+            Map<String, String> result = new HashMap<>();
+            for (String param : query.split("&")) {
+                String[] entry = param.split("=");
+                if (entry.length > 1) {
+                    result.put(entry[0], entry[1]);
+                }else{
+                    result.put(entry[0], "");
+                }
+            }
+            return result;
+        }
+    }
+}
